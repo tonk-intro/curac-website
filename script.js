8e1e33c1aa1295531fd807df553dabb2cc545777
tonk-intro <gammelgul@gammelgul.de>
2024-09-07 20:03:17 +0200

    Nice mobile menu

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const navItems = mobileNav.querySelector("ul");
 showMenuButton.addEventListener("click", (e) => {
   if (!navItems.style.display || navItems.style.display == "none") {
     navItems.style.display = "block";
+    showMenuButton.innerText = "Hide Menu";
   } else {
     navItems.style.display = "none";
+    showMenuButton.innerText = "Show Menu";
   }
 });
